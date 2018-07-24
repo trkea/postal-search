766ea4d47191a99053605ad1ba6838dbd1b1e5ed
trkea <trs1141@yahoo.co.jp>
2018-07-24 23:24:42 +0900

    Reformat code

--- static/js/cityInfo.js
+++ static/js/cityInfo.js
@@ -1,14 +1,13 @@
 window.onload = function() {
 	x = Number.parseFloat(document.getElementById('x').value);
 	y = Number.parseFloat(document.getElementById('y').value);
-var map = L.map('map').setView([y,x], 14);
-
-//OSMレイヤー追加
-L.tileLayer(
-	'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-	{
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
-		maxZoom: 18
-	}
-).addTo(map);
+	var map = L.map('map').setView([y,x], 14);
+	//OSMレイヤー追加
+	L.tileLayer(
+	    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+	    {
+	        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
+	        maxZoom: 18
+	    }
+	).addTo(map);
 }
