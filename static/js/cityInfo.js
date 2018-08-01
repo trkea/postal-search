6019c0aec3bafa828bdd56dd0b986d2d07974daa
trkea <trs1141@yahoo.co.jp>
2018-08-01 22:57:29 +0900

    非同期処理 実装

--- static/js/cityInfo.js
+++ static/js/cityInfo.js
@@ -11,3 +11,42 @@ window.onload = function() {
 	    }
 	).addTo(map);
 }
+
+function getParam() {
+        var url   = location.href;
+        parameters    = url.split("?");
+        params   = parameters[1].split("&");
+        var paramsArray = [];
+        for ( i = 0; i < params.length; i++ ) {
+            neet = params[i].split("=");
+            paramsArray.push(neet[0]);
+            paramsArray[neet[0]] = neet[1];
+        }
+        var categoryKey = paramsArray["key"];
+        return categoryKey;
+	}
+
+$(function() {
+$('#favorite').click(function() {
+  $.get('/postal/favorite',
+      {
+        datatype:'html',
+        data:{
+        'prefecture':$('#prefecture').val(),
+        'city':$('#city').val(),
+        'town':$('#town').val(),
+        'x':$('#x').val(),
+        'y':$('#y').val(),
+        },
+        success:function() {
+            if($('#favorite').css('color') == 'rgb(255, 255, 0)') {
+                $('#favorite').css('color','black')
+            }else{
+                $('#favorite').css('color','yellow')
+
+        }
+           },
+      }
+    );
+});
+});
